c4dcd3925e9432e18d6ac6879ae242227dc9fbd7
Yang Bai <yangb@pansoftware.com.au>
2014-07-10 17:19:14 +1000

    dev of list view

--- app/views/list.jsx
+++ app/views/list.jsx
@@ -1,15 +1,43 @@
 /**
  * @module list view
  */
-define(['backbone'], function(backbone) {
-
-    /**
-     *@class
-     */
-    return backbone.View.extend({
-        initialize: function() {},
-        render: function() {
+define(['backbone', 'react', '../models/PostCollection'], function(Backbone, React, PostCollection) {
+    
+    var PostList = React.createClass({
+        render: function(){
+            return (
+            <div>
+            {this.props.posts.map(
+                function(post){
+                    return (
+                        <article key={post.get('id')} className="post">
+                            <header>
+                                <h1>{post.get('title')}</h1>
+                                <p><time></time></p>
+                            </header>
+                            <div dangerouslySetInnerHTML={{__html:post.get('body')}}>
+                            </div>
+                        </article>
+                    );
+            })}
+            </div>
+            );
             
+        }
+    });
+
+    return Backbone.View.extend({
+        initialize: function() {
+            this.posts = new PostCollection();
+            this.posts.fetch();
+        },
+
+        render: function() {
+            React.renderComponent(
+                <PostList posts={this.posts} />,
+                this.el);
+            
+            return this.$el;
         }
     });
 });
