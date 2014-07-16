253c4998fb9e772557dd312a328137ee481c020d
Yang Bai <yangb@pansoftware.com.au>
2014-07-16 17:05:48 +1000

    added post 'update' mock backend

--- app/backend.js
+++ app/backend.js
@@ -8,22 +8,22 @@ define(['backbone', 'underscore', 'jquery', 'text!./data/posts.json'], function(
     backbone.sync = function(method, model) {
 
         if (model.hasOwnProperty('models')) {
-            
+
             switch (method) {
                 case 'read':
                     model.reset(_posts);
                     break;
-                    
+
                 default:
                     break;
             }
-            
+
         } else {
-            
+            console.log(method);
             switch (method) {
                 case 'read':
 
-                    $.Deferred(function(d) {
+                    return $.Deferred(function(d) {
 
                         d.resolve(_.find(_posts,
                             function(p) {
@@ -35,13 +35,26 @@ define(['backbone', 'underscore', 'jquery', 'text!./data/posts.json'], function(
                         model.set(post);
                     });
 
-                    break;
+                case 'update':
+
+                    return $.Deferred(function(d) {
+
+                        setTimeout(function() {
+                            model.set({
+                                modifiedOn: new Date()
+                            });
+                            d.resolve(model.toJSON());
+                        }, 1500);
+
+                    }).promise().done(function(post) {
+                        model.set(post);
+                    });
 
                 default:
                     break;
 
             }
-            
+
         }
 
     };
