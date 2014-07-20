3a02fb7e3152d3dea638dbac775369be6ca42776
codeboy <me@codeboy.im>
2014-07-20 19:17:37 +1000

    added :create :delete in mocking backend. unified promise resolve to always pass model to done

--- app/backend.js
+++ app/backend.js
@@ -2,56 +2,88 @@
  *@module backend
  */
 
-define(['backbone', 'underscore', 'jquery', 'text!./data/posts.json'], function(backbone, _, $, posts) {
+define(['backbone', 'underscore', 'jquery', 'text!./data/posts.json'], function (backbone, _, $, posts) {
     var _posts = JSON.parse(posts);
 
-    backbone.sync = function(method, model) {
+    backbone.sync = function (method, model) {
 
         if (model.hasOwnProperty('models')) {
 
             switch (method) {
-                case 'read':
-                    model.reset(_posts);
-                    break;
+            case 'read':
+                model.reset(_posts);
+                break;
 
-                default:
-                    break;
+            default:
+                break;
             }
 
         } else {
             console.log(method);
             switch (method) {
-                case 'read':
+            case 'read':
 
-                    return $.Deferred(function(d) {
+                return $.Deferred(function (d) {
 
-                        d.resolve(_.find(_posts,
-                            function(p) {
-                                return p.id === model.get('id');
-                            }
-                        ));
+                    var post = _.find(_posts,
+                        function (p) {
+                            return p.id === model.get('id');
+                        }
+                    );
+                    model.set(post);
+                    d.resolve(model);
 
-                    }).promise().done(function(post) {
-                        model.set(post);
+                }).promise();
+
+            case 'update':
+
+                return $.Deferred(function (d) {
+
+                    var post = _.find(_posts, function (p) {
+                        return p.id === model.get('id');
                     });
 
-                case 'update':
-
-                    return $.Deferred(function(d) {
-
-                        setTimeout(function() {
-                            model.set({
-                                modifiedOn: new Date()
-                            });
-                            d.resolve(model.toJSON());
-                        }, 1500);
-
-                    }).promise().done(function(post) {
-                        model.set(post);
+                    _.extend(post, model.toJSON(), {
+                        modifiedOn: new Date()
                     });
 
-                default:
-                    break;
+                    model.set(post);
+                    d.resolve(model);
+
+                }).promise();
+
+            case 'create':
+
+                return $.Deferred(function (d) {
+                    var post = {};
+                    _.extend(post, model.toJSON(), {
+                        id: _posts[_posts.length - 1].id + 1,
+                        createdOn: new Date()
+                    });
+                    
+                    _posts.push(post);
+                    
+                    model.set(post);
+                    d.resolve(model);
+
+                }).promise();
+
+            case 'delete':
+
+                return $.Deferred(function (d) {
+
+                    var post = _.find(_posts, function (p) {
+                        return p.id === model.get('id');
+                    });
+
+                    _posts.splice(_posts.indexOf(post), 1);
+
+                    d.resolve(model);
+
+                }).promise();
+
+            default:
+                break;
 
             }
 
