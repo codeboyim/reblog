2255b35d9693a2aac1530232d45358e5d083dba0
codeboy <me@codeboy.im>
2014-07-29 02:37:35 +1000

    set user ACL when firstly signing up.

--- app/views/login.js
+++ app/views/login.js
@@ -1,11 +1,11 @@
-define(['react', 'parse', 'backbone', 'underscore', 'globals'], function(React, Parse, Backbone, _, globals) {
+define(['react', 'parse', 'backbone', 'underscore', 'globals'], function (React, Parse, Backbone, _, globals) {
 
     /**
      * @constructor
      * @param {object} options
      *        {Element} options.container
      */
-    var exports = function(options) {
+    var exports = function (options) {
         options = options || {};
         this.container = options.container;
         this._returnUrl = options.returnUrl || '';
@@ -19,29 +19,44 @@ define(['react', 'parse', 'backbone', 'underscore', 'globals'], function(React, 
 
     };
 
-    exports.prototype._loginClicked = function(e) {
+    exports.prototype._loginClicked = function (e) {
         var self = this;
 
         e.preventDefault();
 
         Parse.FacebookUtils.logIn(null, {
 
-            success: function(user) {
-                FB.api(user.get('authData').facebook.id, function(res) {
-                    user.set('name', res.name)
-                        .save();
+            success: function (user) {
+
+                FB.api(user.get('authData').facebook.id, function (res) {
+                    var acl;
+                    
+                    if (user.get('name') !== res.name) {
+                        user.set('name', res.name);
+                    }
+
+                    if (!user.existed()) {
+                        acl = new Parse.ACL();
+                        acl.setRoleReadAccess('Administrators',true);
+                        acl.setPublicReadAccess(false);
+                        user.setACL(acl);
+                    }
+                    
+                    user.save();
+
                 });
+                
                 globals.events
                     .trigger(globals.EVENT.authStatusChanged, true)
                     .trigger(globals.EVENT.authLoggedIn, self._returnUrl);
             },
-            error: function(user, error) {
+            error: function (user, error) {
                 console.error("User cancelled the Facebook login or did not fully authorize.");
             }
         });
     };
 
-    exports.prototype.unload = function() {
+    exports.prototype.unload = function () {
         React.unmountComponentAtNode(this.container);
         this.stopListening();
         globals.events.trigger(globals.EVENT.viewUnloaded, this);
