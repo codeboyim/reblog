2ec9a244c842c24bc82c7b9cd36bda459f6dcc8c
Yang Bai <yangb@pansoftware.com.au>
2014-07-30 14:25:24 +1000

    got scss right with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded',
+                    loadPath: ['lib/vendors/foundation/scss/'],
+                    trace:true
+                },
+                files: {
+                    'public/css/site.css': ['public/scss/site.scss', 'public/scss/_settings.scss']
+                }
+            }
+        },
+        watch: {
+            dev: {
+                files: ['public/scss/site.scss'],
+                tasks: ['sass:dev'],
+                options: {
+                    spawn: false
+                    
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-newer');
+
+    grunt.registerTask('default', ['sass:dev', 'watch']);
+};
