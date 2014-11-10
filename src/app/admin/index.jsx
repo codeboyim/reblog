6eee78e0b3c37d24caba195b1a7eecb936eecbf8
codeboy <me@codeboy.im>
2014-11-11 01:17:11 +1100

    admin sidebar collapse animation

--- src/app/admin/index.jsx
+++ src/app/admin/index.jsx
@@ -9,18 +9,17 @@ module.exports = function(path, ...args){
 
 		case 'new':
 			AdminElm = require('./edit');
-			console.log('new');
 			React.render(<Layout activeMenuItemUid="new"><AdminElm /></Layout>, docBody);
 			break;
 
 		case 'posts':
 			// AdminElm = require('./posts');
-			React.render(<Layout activeMenuItemUid="new"><h2>Posts</h2></Layout>, docBody);
+			React.render(<Layout activeMenuItemUid="posts"><h2>Posts</h2></Layout>, docBody);
 			break	
 
 		case 'drafts':
 			// AdminElm = require('./drafts');
-			React.render(<Layout activeMenuItemUid="new"><h2>Drafts</h2></Layout>, docBody);
+			React.render(<Layout activeMenuItemUid="drafts"><h2>Drafts</h2></Layout>, docBody);
 			break	
 	}
 }
--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -13,33 +13,66 @@ var Layout = React.createClass({
                 'shift': this.state.isSidebarVisible,
                 'adminHeader': true
             });
-
+console.log(this.props.activeMenuItemUid);
         return (
         	<div className="admin" onClick={this._documentBodyClicked}>
                 <header className={cxHeader}>
-                    <i onClick={this._toggleSidebar}>{this.state.isSidebarVisible?'hide':'show'}</i>
+                    <i className="adminSidebarToggler" onClick={this._toggleSidebar}></i>
                     <nav>
                     </nav>
                 </header>
-                <aside className={cxSidebar} ref="adminSidebar">
-                    <a className="logo" href="/">Re/blog</a>
-                    <ul className="adminSidebarNav">
-                        <li key='new' className={'new'===this.props.activeMenuItemUid?'active':''}>
-                            <a href='/a/new'>Write a post</a>
-                            <div></div>
-                        </li>
-                        <li key='posts' className={'posts'===this.props.activeMenuItemUid?'active':''}>
-                            <a href='/a/posts'>Posts</a>
-                        </li>
-                        <li key='drafts' className={'drafts'===this.props.activeMenuItemUid?'active':''}>
-                            <a href='/a/drafts'>Drafts</a>
-                        </li>
+                <aside className={cxSidebar} ref="sidebar">
+                    <a ref="logo" className="logo" href="/">Re/blog</a>
+                    <ul ref="nav" className="adminSidebarNav">
+                        {this._renderMenuItem('new')}
+                        {this._renderMenuItem('posts')}
+                        {this._renderMenuItem('drafts')}
                     </ul>
                 </aside>
                 <div className={cxMain}>
             		{this.props.children}
         		</div>
           </div>
+        );
+    },
+
+    _renderMenuItem(key){
+        var menuItems = {
+                'new': {
+                    'href': '/a/new',
+                    'text': 'Write a post',
+                    'height': 1,
+
+                },
+                'posts': {
+                    'href': '/a/posts',
+                    'text': 'Posts',
+                    'height': 1
+                },
+                'drafts': {
+                    'href': '/a/drafts',
+                    'text': 'Drafts',
+                    'height': 1
+                }
+            },
+            item = menuItems[key];
+
+        if(!item){
+            return null;
+        }
+
+        if(this.props.activeMenuItemUid === key){
+            item.cx = 'active';
+            item.height = this.state.activeContentHeight
+        }
+
+        return (
+            <li key={key} className={item.cx}>
+                <a ref='menuItemTitle' href={ item.href }>{ item.text }</a>
+                <div className='adminMenuItemContent' style={{ height: (item.height + 'px') }}>
+                    {item.content}
+                </div>
+            </li>
         );
     },
 
@@ -51,14 +84,43 @@ var Layout = React.createClass({
 
     getInitialState(){
         return {
-            isSidebarVisible: true
+            isSidebarVisible: true,
+            activeContentHeight: 1
         };
     },
 
     componentDidMount(){
+        this._resizeActiveMenuContent();
+        window.addEventListener('resize', this._resizeActiveMenuContent);
+        document.body.addEventListener('click', this._hideSidebar);
     },
 
-    _documentBodyClicked(e){
+    componentWillUnmount(){
+        window.removeEventListener('resize', this._resizeActiveMenuContent);
+        document.body.removeEventListener('click', this._hideSidebar)
+    },
+
+    _resizeActiveMenuContent(){
+        var menuItemsCount = 3,
+            collapsedContentHeight = 1;
+
+        this._fixedSize = this._fixedSize || 
+            ( 
+                menuItemsCount * this.refs.menuItemTitle.getDOMNode().offsetHeight +
+                (menuItemsCount - 1) * collapsedContentHeight +
+                this.refs.logo.getDOMNode().offsetHeight
+            );
+
+        window.requestAnimationFrame(function(){
+            this.setState({ activeContentHeight: this.refs.sidebar.getDOMNode().offsetHeight - this._fixedSize });
+        }.bind(this));
+    },
+
+    _hideSidebar(e){
+        console.log(e.type);
+        if(this.state.isSidebarVisible && !~[].slice.apply(this.refs.sidebar.getDOMNode().getElementsByTagName(e.target.tagName)).indexOf(e.target)){
+            this._toggleSidebar(false);
+        }
     },
 
     _toggleSidebar(...args){
