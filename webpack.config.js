df6ff15d6fcfb920193b50b2ccd5ed0b3e267aa5
codeboy <me@codeboy.im>
2014-11-11 01:15:34 +1100

    add font-awesome

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 var webpack = require('webpack'),
     path = require('path'),
     neatPath = path.join(__dirname, 'bower_components/neat/app/assets/stylesheets'),
-    bourbonPath = path.join(__dirname, 'bower_components/bourbon/dist');
+    bourbonPath = path.join(__dirname, 'bower_components/bourbon/dist'),
+    faPath = path.join(__dirname, 'node_modules/font-awesome/scss');
 
 module.exports = {
     cache: true,
@@ -26,10 +27,16 @@ module.exports = {
             loader: 'exports?exports.Parse'
         }, {
             test: /\.css$/,
-            loader: 'style!css'
+            loader: 'style!css',
+        }, {
+            test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: "url-loader?limit=10000&minetype=application/font-woff"
+        }, {
+            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: "file-loader"
         }, {
             test: /\.scss$/,
-            loader: 'style!css!sass?includePaths[]=' + neatPath + '&includePaths[]=' + bourbonPath
+            loader: 'style!css!sass?includePaths[]=' + neatPath + '&includePaths[]=' + bourbonPath + '&includePaths[]=' + faPath
         }],
         noParse: []
     },
@@ -46,7 +53,7 @@ module.exports = {
         new webpack.ProvidePlugin({
             // $: 'jquery',
             // jQuery: 'jquery',
-            // _: 'underscore',
+            _: 'underscore',
             // Backbone: 'backbone',
             Parse: 'parse',
             React: 'react/addons'
