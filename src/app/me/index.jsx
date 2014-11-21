c21e67b5139fc7afa520e78fd2dfadb13ec43727
codeboy me <me@codeboy.im>
2014-11-21 12:40:06 +1100

    removed unused leftovers

--- src/app/me/index.jsx
+++
@@ -1,8 +0,0 @@
-var Home = require('./components/home'),
-    Layout = require('shared/_layout');
-
-
-module.exports = function(...args){
-    require('./me.css');
-    React.renderComponent(<Layout><Home/></Layout>, document.body);
-}
--- src/scripts/moment.js
+++
@@ -1,7 +0,0 @@
-var moment = require('./vendors/moment/moment.js'),
-    globals = require('globals');
-//    locale = require('vendors/moment/locale/' + globals.locale);
-
-moment.locale(globals.locale);
-
-module.exports = moment;
