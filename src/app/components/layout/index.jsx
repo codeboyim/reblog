3ec2415368d495bc4a63bfe71a2442095414aadd
codeboy me <me@codeboy.im>
2014-11-24 16:53:41 +1100

    animated hiding header when scrolling

--- src/app/components/layout/index.jsx
+++ src/app/components/layout/index.jsx
@@ -4,15 +4,17 @@ class HomeLayout {
 
     getInitialState() {
         return {
-            hidden: true,
+            hideHeader: false,
             authenticated: !!Parse.User.current()
         };
     }
 
     render() {
+        var cx = React.addons.classSet,
+            cxHeader = cx({'rootHeader': true, 'hide': this.state.hideHeader});
         return (
-            <div className="root">
-                <header className="rootHeader">
+            <div className="root" ref="root" >
+                <header className={cxHeader}>
                     <a href="/"><h1>Re/blog</h1></a>
                     <a href="/a/p/new" title="write a new post" className="rootHeaderButton add flaticon-plus72"></a>
                 </header>
@@ -26,6 +28,32 @@ class HomeLayout {
         );
     }
 
+    componentDidMount(){
+        this.refs.root.getDOMNode().addEventListener('scroll', this._rootScroll());
+    }
+
+    _rootScroll(){
+        var busy = false,
+            lastScrollTop = 0,
+            up = false;
+
+        return (e) =>{
+            var target = e.target;
+            if(!busy){
+
+                    var scrollTop = target.scrollTop;
+                    up = scrollTop - lastScrollTop > 0;
+                    console.log(scrollTop, lastScrollTop);
+                    lastScrollTop = scrollTop;
+                busy = true;
+                window.setTimeout(() => {
+                    busy = false;
+                    this.setState({'hideHeader':up});
+                }, 60);
+            }
+
+        };
+    }
 }
 
 module.exports = React.createClass(HomeLayout.prototype);
