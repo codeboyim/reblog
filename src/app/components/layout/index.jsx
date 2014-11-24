a4eac400d0664523dfff83211e7c92ab3fcde738
codeboy me <me@codeboy.im>
2014-11-24 14:43:17 +1100

    changed home/layout jsx to be using harmony class

--- src/app/components/layout/index.jsx
+++ src/app/components/layout/index.jsx
@@ -1,31 +1,31 @@
 require('./style.scss');
 
-var Layout = React.createClass({
+class HomeLayout {
 
     getInitialState() {
         return {
             hidden: true,
             authenticated: !!Parse.User.current()
         };
-    },
+    }
 
     render() {
         return (
-        	<div className="root">
-        		<header className="rootHeader">
-        			<a href="/"><h1>Re/blog</h1></a>
+            <div className="root">
+                <header className="rootHeader">
+                    <a href="/"><h1>Re/blog</h1></a>
                     <a href="/a/p/new" title="write a new post" className="rootHeaderButton add flaticon-plus72"></a>
-        		</header>
-        		<div className="rootMain">
-        		{this.props.children}
-        		</div>
-        		<footer className="rootFooter">
-	        		<span>Developed by codeboy</span>
-        		</footer>
+                </header>
+                <div className="rootMain">
+                {this.props.children}
+                </div>
+                <footer className="rootFooter">
+                    <span>Developed by codeboy</span>
+                </footer>
           </div>
         );
     }
 
-});
+}
 
-module.exports = Layout;
+module.exports = React.createClass(HomeLayout.prototype);
