e2eace8e1a6f4f0da9f5a0c0de2f6daac728916d
codeboy me <me@codeboy.im>
2014-11-24 17:06:31 +1100

    style tweaks. centered home header to grid width

--- src/app/components/layout/index.jsx
+++ src/app/components/layout/index.jsx
@@ -14,12 +14,14 @@ class HomeLayout {
             cxHeader = cx({'rootHeader': true, 'hide': this.state.hideHeader});
         return (
             <div className="root" ref="root" >
-                <header className={cxHeader}>
-                    <a href="/"><h1>Re/blog</h1></a>
-                    <a href="/a/p/new" title="write a new post" className="rootHeaderButton add flaticon-plus72"></a>
-                </header>
+                <div className={cxHeader}>
+                    <header>
+                        <a href="/"><h1>Re/blog</h1></a>
+                        <a href="/a/p/new" title="write a new post" className="rootHeaderButton add flaticon-plus72"></a>
+                    </header>
+                </div>
                 <div className="rootMain">
-                {this.props.children}
+                    {this.props.children}
                 </div>
                 <footer className="rootFooter">
                     <span>Developed by codeboy</span>
@@ -38,13 +40,12 @@ class HomeLayout {
             up = false;
 
         return (e) =>{
-            var target = e.target;
+            var target = e.target,
+                scrollTop = target.scrollTop;
             if(!busy){
-
-                    var scrollTop = target.scrollTop;
-                    up = scrollTop - lastScrollTop > 0;
-                    console.log(scrollTop, lastScrollTop);
-                    lastScrollTop = scrollTop;
+                up = scrollTop - lastScrollTop > 0;
+                console.log(scrollTop, lastScrollTop);
+                lastScrollTop = scrollTop;
                 busy = true;
                 window.setTimeout(() => {
                     busy = false;
