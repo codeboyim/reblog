3d04cdeaf5b99b752f7abc72f226753ab7a64270
codeboy me <me@codeboy.im>
2014-11-28 10:33:38 +1100

    changed login to be in a form

--- src/app/components/login/index.jsx
+++ src/app/components/login/index.jsx
@@ -21,46 +21,53 @@ class Login{
 	render(){
 		return (
 			<div className="login">
-				<div className="loginBox">
+				<form className="loginForm" onSubmit={this._onFormSubmit}>
 					<div>
 						<label>Login Name</label>
-						<input type="text" onChange={this._inputChanged.bind(this, 'name')} value={this.state.name}/>
+						<input type="text" required onChange={this._onInputChange.bind(this, 'name')} value={this.state.name}/>
 					</div>	
 					<div>
 						<label>Password</label>
-						<input type="password" onChange={this._inputChanged.bind(this, 'password')} value={this.state.password} />
+						<input type="password" required onChange={this._onInputChange.bind(this, 'password')} value={this.state.password} />
 					</div>
 					<div className="text-center">
-						<button className="loginButtonLogin" onClick={this._buttonClicked.bind(this, 'login')}>Login</button>
-						<button className="loginButtonCancel" onClick={this._buttonClicked.bind(this, 'cancel')}>Cancel</button>
+						<input type="submit" className="loginButtonLogin" value="Login" />
+						<button className="loginButtonCancel" onClick={this._onButtonClick.bind(this, 'cancel')}>Cancel</button>
 					</div>
-				</div>
-				{this.state.flashError?<span>{this.state.flashError}</span>:null}
+				</form>
+				{this.state.flashError?<span className="error">{this.state.flashError}</span>:null}
 			</div>
 		);	
 	}
 
-	_buttonClicked(type, e){
+	_onFormSubmit(e){
 		e.preventDefault();
-		if(type === 'cancel'){
-			router.setRoute('/');
-		} else {
-			if(!this.state.name || !this.state.password){
-				this.setState({flashError: 'user name and password are required'});
-			}
-			else{
-				Parse.User.logIn(this.state.name, this.state.password)
-					.done(()=>{
-						router.setRoute(this.props.returnUrl);
-					})
-					.fail(()=>{
-						this.setState({flashError: 'incorrect user name or password'});
-					})
-			}
+
+		if(!this.state.name || !this.state.password){
+			this.setState({flashError: '* user name and password are required'});
 		}
+		else{
+			Parse.User.logIn(this.state.name, this.state.password)
+				.done(()=>{
+					router.setRoute(this.props.returnUrl);
+				})
+				.fail(()=>{
+					this.setState({flashError: '* incorrect user name or password'});
+				})
+		}
+
 	}
 
-	_inputChanged(name, e){
+	_onButtonClick(type, e){
+		e.preventDefault();
+
+		if(type === 'cancel'){
+			router.setRoute('/');
+		} 
+
+	}
+
+	_onInputChange(name, e){
 		var newState = {};
 		newState[name] = e.target.value;
 		this.setState(newState);
